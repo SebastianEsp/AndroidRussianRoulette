94342a7fcd6d606568118359c09c10a572411219
Sebastian Esp Nielsen <sebastianesp@hotmail.com>
2015-04-07 21:17:49 +0200

    Russian Roulette game for android written in java
    Small school assignment meant as an introduction to android development.
    Covers some basic ui interaction in the form of a russian roulette game

---
+++ app/src/main/java/com/example/sebastian/russianroulette/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.sebastian.russianroulette;
+
+import android.graphics.Color;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import java.util.Random;
+import java.lang.*;
+
+
+public class MainActivity extends ActionBarActivity implements View.OnClickListener{
+
+    Random rnd = new Random();
+    Button rndBtn;
+    TextView status;
+    TextView debugInfo;
+    TextView deathCounter;
+    ImageView statusImage;
+    Boolean colorChanger = true;
+    Integer deathCount = 0;
+    String deathCountStr;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        status = (TextView)findViewById(R.id.status);
+        statusImage = (ImageView)findViewById(R.id.statusImage);
+        deathCounter = (TextView)findViewById(R.id.deathCounter);
+        debugInfo = (TextView)findViewById(R.id.debugInfo);
+
+        rndBtn = (Button)findViewById(R.id.rndBtn);
+        rndBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int rndNum = rnd.nextInt(5);
+        debugInfo.setText("Random number is: " + rndNum);
+        if (rndNum == 3)
+        {
+            statusImage.setImageResource(R.drawable.bang);
+            status.setText(getString(R.string.statusDead));
+            status.setBackgroundColor(Color.RED);
+            deathCount+=1;
+            deathCountStr = deathCount.toString();
+            deathCounter.setText(getString(R.string.deathCounter).replaceAll("\\d", deathCountStr));
+        }
+        else if (rndNum != 3)
+        {
+            statusImage.setImageResource(R.drawable.barrel);
+            status.setText(getString(R.string.statusAlive));
+            if (colorChanger == true)
+            {
+                status.setBackgroundColor(Color.BLUE);
+                colorChanger = false;
+            }
+            else if (colorChanger == false)
+            {
+                status.setBackgroundColor(Color.GREEN);
+                colorChanger = true;
+            }
+        }
+    }
+}
